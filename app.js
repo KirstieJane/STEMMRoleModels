08c01fe27d544a2a0f5f56460b45f2b8a2970114
Luke Pacholski <luke.pacholski@gmail.com>
2016-04-06 11:17:39 -0700

    Buncho updates

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 // Then we can use those IDs to show the full versions in teh popup. Woop
 
 // PROTOTYPE TODO - localstorage handling, so you dont have to get new data all the time if you dont want to
-// TODO - should probably add the ID in the cleanupData method... not when it's displayed
+
+// TODO when you've filtered some results, and pop one up, you can still go side to sdie through ALL ofm, should just be the filtered ones.
+
+var dayNames = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+var monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var data;
 
 var dataKeys = {
   "Timestamp" : "event-date",
@@ -25,49 +30,9 @@ var dataKeys = {
   "Your Feedback" : "event-feedback-organizer"
 }
 
-function cleanupData(){
-  for(var i = 0; i < data.length; i++){
-    var item = data[i];
-    for(var k in item) {
-      var itemData = JSON.stringify(item[k]);
-      var newKey = dataKeys[k];
-      delete item[k];
-      item[newKey] = JSON.parse(itemData);
-    }
-  }
-}
-
-function filterEvents(term){
-
-  var matchingIDs = [];
-
-  for(var i = 0; i < data.length; i++){
-    var item = data[i];
-
-    for(var k in item){
-      var value = item[k];
-      if(typeof value ==  "string"){
-        value = value.toLowerCase();
-        if(value.indexOf(term) > -1){
-          matchingIDs.push(item.id);
-        }
-      }
-    }
-  }
-
-
-  $(".event-card").each(function(){
-    $(this).hide();
-    var thisId = parseInt($(this).data("id"));
-    if(matchingIDs.indexOf(thisId) > -1) {
-      $(this).show();
-    }
-  })
-
-
-}
-
 $(document).ready(function(){
+
+  var eventId = parseInt(getUrlParameter("event"));
 
   // if(!localStorage.getItem("data")) {
     $.get("https://sheetsu.com/apis/v1.0/ba3cacae").done(function(returnedData) {
@@ -75,6 +40,10 @@ $(document).ready(function(){
       cleanupData();
       localStorage.setItem("data",JSON.stringify(data));
       displayEvents();
+
+      if(eventId){
+        showPop(eventId);
+      }
     });
   // } else {
   //   data = JSON.parse(localStorage.getItem("data"));
@@ -85,7 +54,6 @@ $(document).ready(function(){
     var term = $(this).val();
     term = term.toLowerCase();
     filterEvents(term);
-
   });
 
   $("body").on("click",".event-card",function(){
@@ -121,9 +89,8 @@ $(document).ready(function(){
 
 });
 
-var dayNames = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-var monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var data;
+// Navigates the event report details popup to either the next
+// or previous event chronologically
 
 function navigatePopup(direction) {
   var popupEl = $(".event-popup-wrapper .event-popup");
@@ -151,6 +118,8 @@ function navigatePopup(direction) {
   }
 }
 
+// Displays a larger popup when an event card is clicked
+// Also changes the browser history, so you can share a link...
 
 function showPop(id){
   var pop = $(".event-popup-wrapper");
@@ -161,27 +130,44 @@ function showPop(id){
     pop.data("id",id);
 
     if(item.id == id) {
+
+      history.replaceState({}, "Mozilla Clubs Event Report " + item.id, "?event=" + item.id);
+
       for(var j in item){
+        if(pop.find("." + j).length > 0){
+          var value = item[j];
 
-      if(pop.find("." + j).length > 0){
-        var value = item[j];
-        if(j == "event-date"){
-          value = formatDate(value);
-        }
-        if(j == "event-attendance"){
-          value = numberWithCommas(value);
-        }
-        pop.find("." + j + " .value").text(value);
-      }
 
+          if(j == "event-date"){
+            value = formatDate(value);
+          }
+          if(j == "event-attendance"){
+            value = numberWithCommas(value);
+          }
+
+          pop.find("." + j + " .value").removeClass("not-specified");
+
+          if(value.length == 0){
+            value = "Not specified";
+            pop.find("." + j + " .value").addClass("not-specified");
+          }
+
+          pop.find("." + j + " .value").text(value);
+        }
       }
     }
   }
+
+  console.log("jamham");
 }
 
 function hidePop(){
+  var clean_uri = location.protocol + "//" + location.host + location.pathname;
+  window.history.replaceState({}, document.title, clean_uri);
   $(".event-popup-wrapper").hide();
 }
+
+// Formats the date from a Date objects to a string like April 2, 2016
 
 function formatDate(dateString) {
   var date = new Date(dateString);
@@ -194,6 +180,8 @@ function formatDate(dateString) {
   return monthName + " " + dayOfMonth + ", " + year;
 }
 
+// Sorts event report objects by date
+
 function dateSort(a,b){
   var dateA = new Date(a["event-date"]);
   var dateB = new Date(b["event-date"]);
@@ -204,8 +192,10 @@ function dateSort(a,b){
   }
 }
 
+// For each event report that is loaded, this adds a little event card to the page
+// with some of the event data
+
 function displayEvents(){
-  data = data.sort(dateSort);
 
   $(".throbber").addClass("goodbye");
   $(".month").show();
@@ -214,17 +204,13 @@ function displayEvents(){
   $(".event-count").text(eventCount);
 
   var participants = 0;
-  var id = 1;
 
   for(var k in data){
     var itemEl = $(".event-card.template").clone();
 
     var item = data[k];
-    item.id = id;
-    itemEl.data("id",id);
-    id++;
 
-
+    itemEl.data("id",item.id);
 
     for(var j in item){
       if(itemEl.find("." + j).length > 0){
@@ -248,9 +234,81 @@ function displayEvents(){
   }
 
   $(".participant-count").text(numberWithCommas(participants));
-
 }
+
+// Returns numbers with commas, so 1000000 becomes 1,000,000
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// * Replaces the original keys in each item's object with the ones above
+// * Adds a row number to each item to match what's in the spreadsheet
+// * Sorts the items according to the date (most recent first);
+
+function cleanupData(){
+  for(var i = 0; i < data.length; i++){
+    var item = data[i];
+    for(var k in item) {
+      var itemData = JSON.stringify(item[k]);
+      var newKey = dataKeys[k];
+      delete item[k];
+      item[newKey] = JSON.parse(itemData);
+    }
+    item.id = i + 2;
+  }
+  data = data.sort(dateSort);
+}
+
+// Hides elements on the page whose details don't contain
+// any matches from the provided search term.
+
+function filterEvents(term){
+
+  var matchingIDs = [];
+
+  for(var i = 0; i < data.length; i++){
+    var item = data[i];
+
+    for(var k in item){
+      var value = item[k];
+      if(typeof value ==  "string"){
+        value = value.toLowerCase();
+        if(value.indexOf(term) > -1){
+          matchingIDs.push(item.id);
+        }
+      }
+    }
+  }
+
+  $(".event-card").each(function(){
+    $(this).hide();
+    var thisId = parseInt($(this).data("id"));
+    if(matchingIDs.indexOf(thisId) > -1) {
+      $(this).show();
+    }
+  });
+
+  if($(".event-card:visible").length == 0){
+    $(".no-results").show();
+  } else {
+    $(".no-results").hide();
+  }
+
+}
+
+
+// Return parameters from the URL, so for example ?id=200 will return 200
+
+function getUrlParameter(sParam){
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++)
+  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam)
+    {
+      return sParameterName[1];
+    }
+  }
 }
