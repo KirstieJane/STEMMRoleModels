389723baefe1479125fc4fb1878f563a03e9cc1a
Gurpreet Kalra <grprtkal@gmail.com>
2016-05-04 12:41:12 -0700

    Created homepage using Yeoman.
    I installed and created the main scaffolding for the app using Yeoman. When deciding on configuration, I used Angular for our front-end framework. For CSS framework, I used Bootstrap. I did not use SASS or LESS, though that may be added later. Using Angular, I created the views for each link on the homepage. I also created a few controllers as a starting point for when we want to use more dynamic data and utilize Angular's data-binding capabilities later.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stemmroleModelsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the stemmroleModelsApp
+ */
+angular.module('stemmroleModelsApp')
+  .controller('AboutCtrl', function ($scope) {
+  	// use $scope object here to add business logic for about view 
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stemmroleModelsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stemmroleModelsApp
+ */
+angular.module('stemmroleModelsApp')
+  .controller('MainCtrl', function ($scope) {
+  	// use $scope object here to add business logic main view 
+  });
